cc96f43d23f490c5ad3505ff2f0757c6258d2954
Gabriel Lopez <gabriellopezdelaossa@gmail.com>
2026-03-16 20:17:38 +0000

    feat: Use environment variables for Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAa8c9D0sGoWfOfDbE1kFyoj4pfkpqccR8",
-  authDomain: "studio-8938243406-4a350.firebaseapp.com",
-  projectId: "studio-8938243406-4a350",
-  storageBucket: "studio-8938243406-4a350.firebasestorage.app",
-  messagingSenderId: "933249296393",
-  appId: "1:933249296393:web:269e1eb1446fcb494b70d8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} 
+
+export { app };
